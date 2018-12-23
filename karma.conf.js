df5056c68ef375354d32723535b59a75b3d019a6
Sindre Wilting <sindrewilting@gmail.com>
2018-12-24 00:03:41 +0100

    Simplify karma and wallaby config for Nodejs only applications

--- karma.conf.js
+++ karma.conf.js
@@ -1,39 +1,23 @@
 "use strict";
-const fs = require("fs");
-const path = require('path');
+const fs = require('fs');
+const babelConfig = JSON.parse(fs.readFileSync('./.babelrc'));
 
-const nodeModulesDir = path.resolve('node_modules');
-let webpackConfig = require('./webpack.specs.config');
 module.exports = (config) => {
-  config.set({
-    basePath: './Features',
-    frameworks: ['mocha', 'chai', 'chai-as-promised'],
-    files: [
-      nodeModulesDir+'/@babel/polyfill/dist/polyfill.js',
-      '**/for_*/*.js',
-      '**/when_*/*.js'
-    ],
-    exclude: [
-    ],
-    preprocessors: {
-      '**/*.js': ['webpack']
-    },
-    webpack: webpackConfig,
-    webpackMiddleware: {
-      stats: 'errors-only'
-    },
-    reporters: ['progress'],
-    coverageIstanbulReporter: {
-      reports: [ 'text-summary' ],
-      fixWebpackSourcePaths: true
-    },    
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['PhantomJS'],
-    singleRun: false,
-    concurrency: Infinity
-  })
+    config.set({
+        frameworks: ['mocha', 'chai', 'chai-as-promised'],
+        preprocessors: {
+            'Source/**/*.js': ['babel']
+        },
+        files: [
+            'Source/**/*.js'
+        ],
+        babelPreprocessor: {
+            options: babelConfig
+        },
+        port: 9876,
+        browsers: ['PhantomJS'],
+        colors: true,
+        reporters: ['progress'],
+        singleRun: true
+    })
 };
-// , 'coverage-istanbul'
