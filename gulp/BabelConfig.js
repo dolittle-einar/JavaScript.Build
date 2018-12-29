1ee7cf622105c49b553d9c8bff9d663d08396fc7
Einar Ingebrigtsen <einar@dolittle.com>
2018-12-29 17:06:35 +0100

    Making private method

--- gulp/BabelConfig.js
+++ gulp/BabelConfig.js
@@ -5,47 +5,6 @@
 import path from 'path';
 import fs from 'fs';
 
-function loadConfig(config) {
-    let configFilenames = [
-        '.babelrc',
-        '.babelrc.js'
-    ];
-
-    let dirname = path.resolve(config.rootFolder);
-
-    let configFile = null;
-
-    while (true) {
-        let found = configFilenames.some(filename => {
-            let fullpath = path.join(dirname, filename)
-            if (fs.existsSync(fullpath)) {
-                configFile = fullpath;
-                
-                return true;
-            }
-        });
-
-        if (found) break;
-
-        const nextDir = path.dirname(dirname);
-        if (dirname === nextDir) break;
-        dirname = nextDir;
-    }
-
-    if (configFile) {
-        console.log(`Using Babel configuration file '${configFile}'`);
-        if (path.extname(configFile) == '.js') return require(configFile);
-        else {
-            let json = fs.readFileSync(configFile);
-            return JSON.parse(json);
-        }
-    }
-    else {
-        console.log('No Babel configuration file found');
-        return {};
-    }
-}
-
 /**
  * Represents a system for handling configuration of Babel
  */
@@ -56,15 +15,62 @@ export class BabelConfig {
      * Initializes a new instance of {babelConfig}
      */
     constructor(config) {
-        this.#baseConfig = loadConfig(config);
+        this.#baseConfig = this.#loadConfig(config);
         this.plugins = [];
         this.presets = [];
     }
 
+    /**
+     * Get a Babel configuration for a specific module format
+     * @param {string} moduleFormat The module format to get config for
+     */
     getConfigForModuleFormat(moduleFormat) {
         let config = {};
         Object.assign(config, this.#baseConfig);
         config.plugins.push(`@babel/plugin-transform-modules-${moduleFormat}`);
         return config;
     }
+
+
+    #loadConfig(config) {
+        let configFilenames = [
+            '.babelrc',
+            '.babelrc.js'
+        ];
+    
+        let dirname = path.resolve(config.rootFolder);
+    
+        let configFile = null;
+    
+        while (true) {
+            let found = configFilenames.some(filename => {
+                let fullpath = path.join(dirname, filename)
+                if (fs.existsSync(fullpath)) {
+                    configFile = fullpath;
+                    
+                    return true;
+                }
+            });
+    
+            if (found) break;
+    
+            const nextDir = path.dirname(dirname);
+            if (dirname === nextDir) break;
+            dirname = nextDir;
+        }
+    
+        if (configFile) {
+            console.log(`Using Babel configuration file '${configFile}'`);
+            if (path.extname(configFile) == '.js') return require(configFile);
+            else {
+                let json = fs.readFileSync(configFile);
+                return JSON.parse(json);
+            }
+        }
+        else {
+            console.log('No Babel configuration file found');
+            return {};
+        }
+    }
+   
 }
